98170f25691ddd0faa8c8d8c3482c9b2ccd5ca76
Cody Averett <codyaverett@gmail.com>
2017-12-19 14:28:12 -0600

    WIP: Adding webpack

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Cody's React Component Lib
+React Component lib for my own personal use and experimentations.
+
+## Reference Material
+* [React Typescript](https://javascriptplayground.com/blog/2017/04/react-typescript/)
+* [Setting up typescript and React](http://blog.tomduncalf.com/posts/setting-up-typescript-and-react/)
--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -2,17 +2,16 @@ import * as React from "react";
 
 interface NavBarProps {
     title: string;
-    children?: any
 }
 
-export class NavBar extends React.Component<NavBarProps, {}> {
+export default class NavBar extends React.Component {
     constructor(props: NavBarProps) {
         super(props);
     }
 
     render() {
         return (
-            <div> hallo </div>
+            <div> I'm an App </div>
         );
     }
 }
